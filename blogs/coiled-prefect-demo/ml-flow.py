a03c5c845169cb12980be68157f7279121be5f3d
Kevin Kho <kdykho@gmail.com>
2021-05-04 11:00:44 -0400

    ml flow

---
+++ blogs/coiled-prefect-demo/ml-flow.py
@@ -0,0 +1,61 @@
+import dask.dataframe as dd
+from distributed import wait
+from dask_ml.model_selection import train_test_split
+from dask_ml.xgboost import XGBRegressor
+from dask_ml.metrics import mean_absolute_error
+
+from prefect import Flow, task
+import prefect
+
+@task(name="Load Data")
+def load_data():
+    df = dd.read_csv(
+        "s3://nyc-tlc/trip data/yellow_tripdata_2019-01.csv",
+        dtype={
+            "payment_type": "UInt8",
+            "VendorID": "UInt8",
+            "passenger_count": "UInt8",
+            "RatecodeID": "UInt8",
+        },
+        storage_options={"anon": True}
+    )
+    df = df[['payment_type', 'fare_amount', 'trip_distance', 'passenger_count', 'tpep_pickup_datetime', 'tip_amount']]
+    return df
+
+@task(name="Create Features")
+def create_features(df: dd.DataFrame) -> dd.DataFrame:
+    # Create hour of day feature
+    df['hour'] = dd.to_datetime(df["tpep_pickup_datetime"],unit='ns').dt.hour
+    df = df.drop("tpep_pickup_datetime", axis=1)
+    return df
+
+@task(name="Print Head")
+def print_head(df: dd.DataFrame) -> dd.DataFrame:
+    print(df.head())
+    return df
+
+@task(name="Train Model")
+def train_model(df: dd.DataFrame):
+    train, test = train_test_split(df, test_size = 0.2)
+
+    est = XGBRegressor(params = {'objective': 'reg:squarederror',
+          'max_depth': 4, 'eta': 0.01, 'min_child_weight': 0.5})
+    est.fit(train.drop('tip_amount', axis=1), train['tip_amount'])
+    
+    y_test = test['tip_amount']
+    y_pred = est.predict(test.drop('tip_amount', axis = 1))
+    metric = mean_absolute_error(y_test, y_pred)
+
+    logger = prefect.context.get("logger")
+    logger.info(f"The mean absolute error is: {metric}")
+
+    return
+
+
+
+with Flow("ml-flow") as flow:
+    df = load_data()
+    df = create_features(df)
+    train_model(df)
+
+flow.run()
