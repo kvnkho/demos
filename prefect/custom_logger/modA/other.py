c0e01c6ac5e324fb19379f4686a8bcde2dd26df8
Kevin Kho <kdykho@gmail.com>
2021-08-23 14:19:23 -0400

    ecs stuff

--- prefect/custom_logger/modA/other.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-# create logger
-module_logger = logging.getLogger(__name__) 
-
-def some_function():
-    module_logger.debug('message from auxiliary module')
---
+++ prefect/ecs/ecs_flow.py
@@ -0,0 +1,28 @@
+from prefect import task, Flow
+from prefect.run_configs import ECSRun
+from prefect.storage import S3
+
+@task
+def abc():
+    return "hello"
+
+with Flow("ecs_test") as flow:
+    abc()
+
+RUN_CONFIG = ECSRun(task_definition_path="test.yaml", 
+                            labels=['ecs_test_c','ecs_test_d'],
+                            run_task_kwargs={'cluster': 'test-cluster'},
+                            task_role_arn= 'arn:aws:iam::12345678:role/prefect-ecs',
+                            execution_role_arn='arn:aws:iam::12345678:role/prefect-ecs',
+                            image= 'prefecthq/prefect:latest-python3.8'
+                            )
+
+flow.storage=S3(bucket="coiled-prefect", add_default_labels=False)
+
+flow.run_config = RUN_CONFIG 
+
+flow.register("dsdc")
+
+"""
+prefect agent ecs start --cluster arn:aws:ecs:us-east-2:029864677731:cluster/test-cluster --label ecs_test_c --label ecs_test_d
+"""
