a9b9ca2e379402184a22da6658a2ec97f31fa239
Kevin Kho <kdykho@gmail.com>
2021-09-08 12:29:10 -0400

    test

--- prefect/testing_reregistration.py
+++ prefect/testing_reregistration.py
@@ -1,6 +1,7 @@
 from prefect import task, Flow
 import time
 from prefect.run_configs import KubernetesRun
+from prefect.storage.github import GitHub
 
 @task
 def abc(x):
@@ -10,6 +11,9 @@ def abc(x):
 with Flow("timer flow") as flow:
     abc.map([1]*10)
 
-flow.storage = Github
+flow.storage = GitHub(
+repo="kvnkho/demos", 
+path="/prefect/testing_reregistration.py",
+ref="main")
 flow.run_config = KubernetesRun()
 flow.register("dsdc")
