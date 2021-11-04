63ad8ad7c028646151fc00800f8b12dec2faa5c2
Kevin Kho <kdykho@gmail.com>
2021-11-04 12:17:30 -0400

    Create host_config.py

---
+++ prefect/host_config.py
@@ -0,0 +1,22 @@
+from prefect import Flow, task
+from prefect.run_configs.docker import DockerRun
+from prefect.storage import GitHub
+
+@task
+def abc():
+    import time
+    time.sleep(60)
+    return 1
+
+with Flow("host_config") as flow:
+    abc()
+
+run_config = DockerRun(host_config=dict(mem_limit="128m"))
+flow.run_config = run_config
+
+flow.storage = GitHub(
+repo="kvnkho/demos", 
+path="/prefect/host_config.py",
+ref="main")
+
+flow.register("bristech")
