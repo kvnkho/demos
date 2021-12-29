62cd1f596e2a7203eee7a4d71c20cff2d56c1aac
Kevin Kho <kdykho@gmail.com>
2021-12-29 04:15:06 -0500

    Update benchmark.py

--- prefect/dask_issues/benchmark.py
+++ prefect/dask_issues/benchmark.py
@@ -34,8 +34,7 @@ with Flow("map_testing") as flow:
     
 executor=DaskExecutor(
         cluster_class=lambda: KubeCluster(pod_template=make_pod_spec(image="prefecthq/prefect",
-        env={'EXTRA_PIP_PACKAGES': "bokeh"}, memory_request='4G', memory_limit='4G', 
-        threads_per_worker=4), n_workers=5),
+        env={'EXTRA_PIP_PACKAGES': "bokeh"}), n_workers=5),
         debug=True,
         performance_report_path="performance_report.html",
         client_kwargs=dict(set_as_default=True)
