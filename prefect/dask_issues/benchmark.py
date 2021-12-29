f31545ed1ccf63a951191d2a3cc235469363a52e
Kevin Kho <kdykho@gmail.com>
2021-12-28 21:39:55 -0500

    Update benchmark.py

--- prefect/dask_issues/benchmark.py
+++ prefect/dask_issues/benchmark.py
@@ -11,17 +11,17 @@ from prefect.utilities.aws import get_boto_client
 import prefect
 
 def custom_terminal_state_handler(
-	flow: Flow,
-	state: State,
-	reference_task_states: Set[State],
+    flow: Flow,
+    state: State,
+    reference_task_states: Set[State],
 ) -> Optional[State]:
-  prefect.context.logger.info("AM AT TERMINAL STATE HANDLER")
-  report = flow.executor.performance_report
-  s3_client = get_boto_client("s3")
-  report_data = io.BytesIO(report.encode())
-  prefect.context.logger.info("UPLOADING THE PERFORMANCE REPORT")
-  s3_client.upload_fileobj(report_data, Bucket="coiled-prefect", Key="performance_report.html")
-  prefect.context.logger.info("DONE UPLOADING")
+    prefect.context.logger.info("AM AT TERMINAL STATE HANDLER")
+    report = flow.executor.performance_report
+    s3_client = get_boto_client("s3")
+    report_data = io.BytesIO(report.encode())
+    prefect.context.logger.info("UPLOADING THE PERFORMANCE REPORT")
+    s3_client.upload_fileobj(report_data, Bucket="coiled-prefect", Key="performance_report.html")
+    prefect.context.logger.info("DONE UPLOADING")
 
 @task
 def do_nothing(n):
