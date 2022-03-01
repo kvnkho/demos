54121f6823514da4c9288a306f05a05b48bd7ae5
Kevin Kho <kdykho@gmail.com>
2022-03-01 10:40:58 -0500

    sync

--- prefect/ecs_flow_memory.py
+++ prefect/ecs_flow_memory.py
@@ -1,25 +1,29 @@
 import prefect
-from prefect import Flow, task
-from prefect.client.secrets import Secret
 from prefect.storage import GitHub
 from prefect.run_configs import ECSRun
+from prefect import task, Flow
 
 
-STORAGE = 
+STORAGE = GitHub(
+repo="kvnkho/demos", 
+path="/prefect/ecs_flow_memory.py",
+ref="main")
 
 RUN_CONFIG = ECSRun(
-    labels=["prod"],
-    task_definition_path="s3://prefectdata/flow_task_definition.yaml",
-    run_task_kwargs=dict(cluster="prefectEcsCluster"),
+    cpu="1 vcpu",  
+    memory="8 GB",
+    task_definition_path="s3://coiled-prefect/flow_task_definition.yaml",
+    run_task_kwargs=dict(cluster="test-cluster"),
 )
-
 
 @task(log_stdout=True)
 def hello_world():
-    text = f"hello from {FLOW_NAME} from Prefect version {prefect.__version__}"
+    text = f"hello from Prefect version {prefect.__version__}"
     print(text)
     return text
 
 
-with Flow(FLOW_NAME, storage=STORAGE, run_config=RUN_CONFIG,) as flow:
+with Flow("ecs-memory-test", storage=STORAGE, run_config=RUN_CONFIG,) as flow:
     hw = hello_world()
+
+flow.register("general_assembly")
