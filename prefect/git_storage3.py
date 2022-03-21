7dfa13bc35d92825a4fcabce22d9f5475e395390
Kevin Kho <kdykho@gmail.com>
2022-03-21 12:19:52 -0400

    Update git_storage3.py

--- prefect/git_storage3.py
+++ prefect/git_storage3.py
@@ -1,30 +1,27 @@
 from prefect import Flow, task
+import time
+from prefect.run_configs import KubernetesRun
+from prefect.engine.state import State, Cancelled
+import prefect
 from prefect.storage import GitHub
-from prefect.run_configs import DockerRun
-from prefect.storage.git import Git
-import os
-from pathlib import Path
 
-FLOW_NAME = "docker_script"
+def post_to_slack_handler_fq(flow: Flow, old_state: State, new_state: State) -> State:
+    if isinstance(new_state, Cancelled):
+        prefect.context.logger.info("HIT THE CANCELLED STATE HANDLER")
+    return new_state
+
+@task(state_handlers=[post_to_slack_handler_fq])
+def abc(x):
+    time.sleep(10)
+    return 1
+
+with Flow("sleep", state_handlers=[post_to_slack_handler_fq]) as flow:
+    abc.map([1,2,3])
 
 storage = GitHub(repo="kvnkho/demos", 
-                path="/prefect/git_storage3.py",
-                ref="main")
+            path="/prefect/git_storage3.py",
+            ref="main")
 
-@task(log_stdout=True)
-def create_directory():
-    text = f"..."
-    local_path = os.path.join("/app/data")
-
-    Path(local_path).mkdir(parents=True, exist_ok=True)
-    return text
-
-@task(log_stdout=True)
-def create_directory2():
-    raise ValueError()
-
-with Flow(FLOW_NAME,storage=storage,run_config=DockerRun()) as flow:
-    create_dir = create_directory()
-    create_directory2()
-
-flow.register(project_name="databricks")
+flow.storage = storage
+flow.run_config = KubernetesRun(image="prefecthq/prefect:latest-python3.8")
+flow.register("databricks")
