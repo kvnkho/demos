48f89afaf1f1666ef08fc9f143fd4e58cef6379d
Kevin Kho <kdykho@gmail.com>
2022-03-21 13:43:17 -0400

    Update git_storage3.py

--- prefect/git_storage3.py
+++ prefect/git_storage3.py
@@ -5,17 +5,22 @@ from prefect.engine.state import State, Cancelled
 import prefect
 from prefect.storage import GitHub
 
-def post_to_slack_handler_fq(flow: Flow, old_state: State, new_state: State) -> State:
+def post_to_slack_handler_task(obj, old_state: State, new_state: State) -> State:
     if isinstance(new_state, Cancelled):
-        prefect.context.logger.info("HIT THE CANCELLED STATE HANDLER")
+        prefect.context.logger.info("HIT THE CANCELLED STATE HANDLER FROM TASK")
     return new_state
 
-@task(state_handlers=[post_to_slack_handler_fq])
+def post_to_slack_handler_flow(obj, old_state: State, new_state: State) -> State:
+    if isinstance(new_state, Cancelled):
+        prefect.context.logger.info("HIT THE CANCELLED STATE HANDLER FROM FLOW")
+    return new_state
+
+@task(state_handlers=[post_to_slack_handler_task])
 def abc(x):
     time.sleep(10)
     return 1
 
-with Flow("sleep", state_handlers=[post_to_slack_handler_fq]) as flow:
+with Flow("sleep", state_handlers=[post_to_slack_handler_flow]) as flow:
     abc.map([1,2,3])
 
 storage = GitHub(repo="kvnkho/demos", 
