acdc4e731fa6b2ccdb40060437ede53e46500321
Kevin Kho <kdykho@gmail.com>
2022-05-09 18:27:46 -0400

    Update ecs_log.py

--- prefect/ecs/ecs_log.py
+++ prefect/ecs/ecs_log.py
@@ -1,6 +1,6 @@
 from prefect import task, Flow
 from prefect.run_configs import ECSRun, DockerRun
-from prefect.storage import S3
+from prefect.storage import S3, GitHub
 from prefect.executors import LocalDaskExecutor
 import prefect
 
@@ -10,8 +10,11 @@ def abc(x):
     print(x)
     return "hello"
 
-with Flow("ecs_test", run_config=DockerRun(), storage=S3(bucket="coiled-prefect", add_default_labels=False, secrets=["AWS_CREDENTIALS"]),
-           executor = LocalDaskExecutor(scheduler="processes")) as flow:
+with Flow("ecs_test", run_config=DockerRun(), executor = LocalDaskExecutor(scheduler="processes")) as flow:
     abc.map(list(range(10)))
 
+flow.storage = GitHub(
+repo="kvnkho/demos", 
+path="/prefect/ecs/ecs_log.py",
+ref="main")
 flow.register("databricks")
